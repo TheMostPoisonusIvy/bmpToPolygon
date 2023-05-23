688ba160f36a224745ad4b32aed53a68805de24a
TheMostPoisonusIvy <107717096+TheMostPoisonusIvy@users.noreply.github.com>
2023-05-23 12:37:35 +0200

    jzrd

---
+++ bmpToPolygon.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class bmpToPolygon extends JFrame {
+    private int[][] bmpDaten;
+    private String inputPathString, outPutpathString;
+
+    // Konstruktor
+    public bmpToPolygon() {
+        super();
+    }
+
+}
