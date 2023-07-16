e75d16e2c3e1b0ce4cb508297c2e9a32611882ac
TheMostPoisonusIvy <107717096+TheMostPoisonusIvy@users.noreply.github.com>
2023-07-16 14:17:32 +0200

    stuff

--- src/picksel.java
+++ src/picksel.java
@@ -2,13 +2,16 @@ package src;
 
 public class picksel {
     int xKoordinate, yKoordinate, wert;
+    int xKoordinateWoher, yKoordinateWoher;
 
     public picksel() {
     }
 
-    public picksel(int x, int y, int w) {
+    public picksel(int x, int y, int w, int xW, int yW) {
         this.xKoordinate = x;
         this.yKoordinate = y;
         this.wert = w;
+        this.xKoordinateWoher = xW;
+        this.yKoordinateWoher = yW;
     }
 }
--- src/pickselToPolygon.java
+++ src/pickselToPolygon.java
@@ -5,19 +5,39 @@ import java.util.Vector;
 public class pickselToPolygon {
     public bitmapToPicksel bmpToPick;
     public Vector<Vector<picksel>> groupsPerRow;
+    int x, y;
 
     public pickselToPolygon(bitmapToPicksel b) {
         this.bmpToPick = b;
-        this.groupsPerRow = new Vector<Vector<picksel>>(b.height);
+        this.groupsPerRow = new Vector<Vector<picksel>>(1);
         System.out.println("Success");
-        // durchgehen einer jeden Bildzeile
-        // finden des Anfangs und Ende einer jeden Pixelgruppe pro Zeile
-        // Zusammenfügen der Gruppen über Zeilen hinweg
+        // beginnen beim obersten linken Pixel
+
+        // Bilden einer Gruppe
+
+        // speichern der Gruppe
+
+        // nordwestlichsten Pixel ohne Gruppe suchen
+
+        // Bilden einer Gruppe usw.
+
         // Erkennen der Kanten der Gruppen
         // Abspeichern einer jeden Gruppe, bzw. deren Kanten, als Polygon
     }
 
-    public void searchForPickselWithSameValueNearTheCurrentOne() {
+    public void addVectorToGeneralVector() {
+
+    }
+
+    public void searchForNewPixelGroupMostNorthWestOfCurrentOne() {
+
+    }
+
+    public void findMostNorthWestCornerOfPickselVector() {
+
+    }
+
+    public void searchForPickselWithSameValueNearTheCurrentOne(picksel p) {
 
     }
 }
